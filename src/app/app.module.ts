1770dd33cbaff8b8f30487b3a001229e2dd9eb40
NEATShonen <panoleous@gmail.com>
2024-10-30 13:35:49 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { NavbarComponent } from './navbar/navbar.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProjectComponent } from './project/project.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { FirestoreModule } from '@angular/fire/firestore';
 import { AngularFirestoreModule, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { MarkdownModule } from 'ngx-markdown'; 
+import { MarkdownModule } from 'ngx-markdown';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
 import { MainComponent } from './main/main.component';
-import { AdminComponent } from './admin/admin.component'; 
+import { AdminComponent } from './admin/admin.component';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -45,11 +46,12 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
     }),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
-    AngularFirestoreModule,    
-    MarkdownModule.forRoot() 
+    AngularFirestoreModule,
+    MarkdownModule.forRoot()
   ],
-  providers: [       
-     { provide: FIREBASE_OPTIONS, useValue: environment.firebase }
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    { provide: APP_BASE_HREF, useValue: "https://github.com/panospetridisoglou/panospetridisoglou.github.io/" }
   ],
   bootstrap: [MainComponent],
   schemas: [
